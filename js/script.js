33ce2662dc2122c5cf038edcf775b987df1b245b
Kamil Huńczak <hunczak.kamil@gmail.com>
2022-08-16 18:51:48 +0200

    add class .active to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 'use strict';
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
+    console.log(event.currentTarget);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
+
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
+
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
-
-    /* remove class 'active' from all articles */
 
     /* get 'href' attribute from the clicked link */
 
